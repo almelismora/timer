d555d8c27adeda108355a2753066ed766ac1c6a3
Elis Mora <moralmedina8@gmail.com>
2024-01-15 22:08:03 +0100

    index.html del componente

---
+++ library/timer/index.js
@@ -0,0 +1,7 @@
+export * from './TimerComponent.js'
+export * from './TimerPartComponent.js'
+export * from './TimerPlayerComponent.js'
+
+// export { TimerComponent } from './TimerComponent.js'
+// export { TimerPlayerComponent } from './TimerPlayerComponent.js'
+// export { TimerPartComponent } from './TimerPartComponent.js'
